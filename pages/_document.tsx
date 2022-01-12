0513f9f4ea72c1863e0a70759ec96541ee25c303
fayd <faisal.sayed502@gmail.com>
2022-01-12 22:21:39 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,23 +11,23 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					<link rel="icon" href="/firefiles-logo.png" type="image/png" />
-					<meta name="title" content="firefiles" />
-					<meta name="description" content="Get your own file hosting service in minutes." />
+					<meta name="title" content="Firefiles" />
+					<meta name="description" content="Get your own personal file hosting service in minutes. Firefiles provides you with a drive like interface for your storage buckets." />
 					<meta property="og:type" content="website" />
-					<meta property="og:url" content="https://firefiles.vercel.app" />
-					<meta property="og:title" content="firefiles" />
-					<meta property="og:description" content="Get your own file hosting service in minutes." />
-					<meta property="og:image" content="https://firefiles.vercel.app/firefiles-preview.png" />
+					<meta property="og:url" content="https://usefirefiles.vercel.app" />
+					<meta property="og:title" content="Firefiles" />
+					<meta property="og:description" content="Get your own personal file hosting service in minutes. Firefiles provides you with a drive like interface for your storage buckets." />
+					<meta property="og:image" content="https://usefirefiles.vercel.app/firefiles-preview.png" />
 					<meta property="twitter:card" content="summary_large_image" />
-					<meta property="twitter:url" content="https://firefiles.vercel.app" />
-					<meta property="twitter:title" content="firefiles" />
+					<meta property="twitter:url" content="https://usefirefiles.vercel.app" />
+					<meta property="twitter:title" content="Firefiles" />
 					<meta
 						property="twitter:description"
-						content="Get your own file hosting service in minutes."
+						content="Get your own personal file hosting service in minutes. Firefiles provides you with a drive like interface for your storage buckets."
 					/>
 					<meta
 						property="twitter:image"
-						content="https://firefiles.vercel.app/firefiles-preview.png"
+						content="https://usefirefiles.vercel.app/firefiles-preview.png"
 					/>
 				</Head>
 				<body>
