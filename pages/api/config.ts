146009b9db138c3b5683e6f9b7e2e7c3503dc618
faisalsayed10 <faisal.sayed502@gmail.com>
2022-01-27 13:11:14 +0530

    :ambulance: (cache) problem solved. it was caching wth.

--- pages/api/config.ts
+++ pages/api/config.ts
@@ -14,7 +14,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 				const encrypted = response.data.fields.config.stringValue;
 				const bytes = AES.decrypt(encrypted, process.env.CIPHER_KEY).toString(enc.Utf8);
 
-				res.setHeader("Cache-Control", "s-maxage=300");
 				return res.status(200).json(JSON.parse(bytes));
 			case "POST":
 				const data = req.body;
