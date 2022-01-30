db8313784cffd22062624fbad979a4c9149ba585
faisalsayed10 <faisal.sayed502@gmail.com>
2022-01-30 11:55:15 +0530

    :bug: (fixing-deployment) fixing private key on vercel

--- util/firebase-admin.ts
+++ util/firebase-admin.ts
@@ -4,7 +4,7 @@ if (admin.apps.length === 0) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-			privateKey: process.env.FIREBASE_PRIVATE_KEY,
+			privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 			projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
 		})
 	});
