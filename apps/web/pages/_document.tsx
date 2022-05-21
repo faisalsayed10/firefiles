c4889de3239d27a7c393cdeba4460a6abcbc29d4
faisalsayed10 <faisal.sayed502@gmail.com>
2022-05-21 10:42:34 +0530

    :pencil2: (typo fix) this is what happens when you have similar names

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -14,14 +14,14 @@ class MyDocument extends Document {
 					<meta name="title" content="Firefiles" />
 					<meta
 						name="description"
-						content="The open-source alternative to Dropbox. Firebase gives you the freedom to bring your own cloud and provides you with a modern file-system interface."
+						content="The open-source alternative to Dropbox. Firefiles gives you the freedom to bring your own cloud and provides you with a modern file-system interface."
 					/>
 					<meta property="og:type" content="website" />
 					<meta property="og:url" content="https://usefirefiles.vercel.app" />
 					<meta property="og:title" content="Firefiles" />
 					<meta
 						property="og:description"
-						content="The open-source alternative to Dropbox. Firebase gives you the freedom to bring your own cloud and provides you with a modern file-system interface."
+						content="The open-source alternative to Dropbox. Firefiles gives you the freedom to bring your own cloud and provides you with a modern file-system interface."
 					/>
 					<meta
 						property="og:image"
@@ -32,7 +32,7 @@ class MyDocument extends Document {
 					<meta property="twitter:title" content="Firefiles" />
 					<meta
 						property="twitter:description"
-						content="The open-source alternative to Dropbox. Firebase gives you the freedom to bring your own cloud and provides you with a modern file-system interface."
+						content="The open-source alternative to Dropbox. Firefiles gives you the freedom to bring your own cloud and provides you with a modern file-system interface."
 					/>
 					<meta
 						property="twitter:image"
