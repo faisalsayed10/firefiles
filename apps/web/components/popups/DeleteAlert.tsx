cf5a19ee074e7340d4c467a905b53d09f37fc40c
faisalsayed10 <faisal.sayed502@gmail.com>
2023-04-10 00:22:39 +0530

    :boom: (tailwind) migrate the popups

--- apps/web/components/popups/DeleteAlert.tsx
+++ apps/web/components/popups/DeleteAlert.tsx
@@ -1,13 +1,6 @@
-import {
-	AlertDialog,
-	AlertDialogBody,
-	AlertDialogContent,
-	AlertDialogFooter,
-	AlertDialogHeader,
-	AlertDialogOverlay,
-	Button,
-} from "@chakra-ui/react";
-import React from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import { Fragment } from "react";
 
 interface Props {
 	isOpen: boolean;
@@ -18,26 +11,74 @@ interface Props {
 
 const DeleteAlert: React.FC<Props> = ({ isOpen, onClick, onClose, cancelRef }) => {
 	return (
-		<AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
-			<AlertDialogOverlay>
-				<AlertDialogContent>
-					<AlertDialogHeader fontSize="lg" fontWeight="bold">
-						Delete Files
-					</AlertDialogHeader>
+		<Transition.Root show={isOpen} as={Fragment}>
+			<Dialog as="div" className="relative z-10" initialFocus={cancelRef} onClose={onClose}>
+				<Transition.Child
+					as={Fragment}
+					enter="ease-out duration-300"
+					enterFrom="opacity-0"
+					enterTo="opacity-100"
+					leave="ease-in duration-200"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
+				>
+					<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+				</Transition.Child>
 
-					<AlertDialogBody>Are you sure? You can't undo this action afterwards.</AlertDialogBody>
-
-					<AlertDialogFooter>
-						<Button ref={cancelRef} onClick={onClose}>
-							Cancel
-						</Button>
-						<Button colorScheme="red" onClick={onClick} ml={3}>
-							Delete
-						</Button>
-					</AlertDialogFooter>
-				</AlertDialogContent>
-			</AlertDialogOverlay>
-		</AlertDialog>
+				<div className="fixed inset-0 z-10 overflow-y-auto">
+					<div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+						<Transition.Child
+							as={Fragment}
+							enter="ease-out duration-300"
+							enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+							enterTo="opacity-100 translate-y-0 sm:scale-100"
+							leave="ease-in duration-200"
+							leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+							leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+						>
+							<Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+								<div className="sm:flex sm:items-start">
+									<div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+										<ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+									</div>
+									<div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+										<Dialog.Title
+											as="h3"
+											className="text-base font-semibold leading-6 text-gray-900"
+										>
+											Delete file
+										</Dialog.Title>
+										<div className="mt-2">
+											<p className="text-sm text-gray-500">
+												Are you sure you want to delete this file? All of the contents will be
+												permanently removed from the servers. This action cannot be undone.
+											</p>
+										</div>
+									</div>
+								</div>
+								<div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+									<button
+										type="button"
+										className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+										onClick={onClick}
+									>
+										Delete
+									</button>
+									<button
+										type="button"
+										className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+										onClick={onClose}
+										ref={cancelRef}
+									>
+										Cancel
+									</button>
+								</div>
+							</Dialog.Panel>
+						</Transition.Child>
+					</div>
+				</div>
+			</Dialog>
+		</Transition.Root>
 	);
 };
 
--- apps/web/components/popups/OptionsPopover.tsx
+++ apps/web/components/popups/OptionsPopover.tsx
@@ -1,19 +1,6 @@
-import {
-	IconButton,
-	Popover,
-	PopoverArrow,
-	PopoverBody,
-	PopoverCloseButton,
-	PopoverContent,
-	PopoverFooter,
-	PopoverHeader,
-	PopoverTrigger,
-	Portal,
-	Text,
-	useColorModeValue,
-} from "@chakra-ui/react";
-import React from "react";
-import { DotsVertical } from "tabler-icons-react";
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Fragment } from "react";
 
 interface Props {
 	header: string;
@@ -22,30 +9,40 @@ interface Props {
 
 const OptionsPopover: React.FC<Props> = ({ header, footer, children }) => {
 	return (
-		<Popover isLazy>
-			<PopoverTrigger>
-				<IconButton aria-label="dots-vertical" variant="link" size="xs">
-					<DotsVertical width="20" height="20" color={useColorModeValue("black", "white")} />
-				</IconButton>
-			</PopoverTrigger>
-			<Portal>
-				<PopoverContent w="250px">
-					<PopoverArrow />
-					<PopoverHeader>
-						<Text fontSize="sm" fontWeight="semibold" isTruncated={true}>
-							{header}
-						</Text>
-					</PopoverHeader>
-					<PopoverCloseButton />
-					<PopoverBody p="0">{children}</PopoverBody>
-					{footer && (
-						<PopoverFooter fontSize="xs" fontWeight="600" fontStyle="italic">
+		<div className="fixed top-16 w-full max-w-sm px-4">
+			<Popover className="relative">
+				{({ open }) => (
+					<>
+						<Popover.Button
+							className={`
+                ${open ? "" : "text-opacity-90"}
+                group inline-flex items-center rounded-md bg-orange-700 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+						>
+							<span>{header}</span>
+							<ChevronDownIcon
+								className={`${open ? "" : "text-opacity-70"}
+                  ml-2 h-5 w-5 text-orange-300 transition duration-150 ease-in-out group-hover:text-opacity-80`}
+								aria-hidden="true"
+							/>
+						</Popover.Button>
+						<Transition
+							as={Fragment}
+							enter="transition ease-out duration-200"
+							enterFrom="opacity-0 translate-y-1"
+							enterTo="opacity-100 translate-y-0"
+							leave="transition ease-in duration-150"
+							leaveFrom="opacity-100 translate-y-0"
+							leaveTo="opacity-0 translate-y-1"
+						>
+							<Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+								{children}
+							</Popover.Panel>
 							{footer}
-						</PopoverFooter>
-					)}
-				</PopoverContent>
-			</Portal>
-		</Popover>
+						</Transition>
+					</>
+				)}
+			</Popover>
+		</div>
 	);
 };
 
