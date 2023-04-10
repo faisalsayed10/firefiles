5d590daa29af9373fcfd7167ff945ff27033f481
faisalsayed10 <faisal.sayed502@gmail.com>
2023-04-11 02:48:44 +0530

    :bug: (bug) small bug fix

--- apps/web/components/ui/AWSRegionSelect.tsx
+++ apps/web/components/ui/AWSRegionSelect.tsx
@@ -48,7 +48,7 @@ const AWSRegionSelect: React.FC<Props> = ({ value, onChange }) => {
 					</Listbox.Label>
 					<div className="relative mt-2">
 						<Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-							<span className="block truncate">{regions.find((r) => r.code === value).name}</span>
+							<span className="block truncate">{regions.find((r) => r.code === value)?.name}</span>
 							<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
 								<ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
 							</span>
@@ -81,7 +81,7 @@ const AWSRegionSelect: React.FC<Props> = ({ value, onChange }) => {
 														"block truncate"
 													)}
 												>
-													{region.name}
+													{region?.name}
 												</span>
 
 												{selected ? (
