26c2bfbe98ef19b543667672e264ec147ea7c185
faisalsayed10 <faisal.sayed502@gmail.com>
2023-04-28 23:53:16 +0530

    :sparkles: (layout) Some work on the new app layout

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,3 +1,4 @@
+import Layout from "@components/Layout";
 import OptionsPopover from "@components/popups/OptionsPopover";
 import AddDriveButton from "@components/ui/AddDriveButton";
 import Navbar from "@components/ui/Navbar";
@@ -6,7 +7,6 @@ import { Drive } from "@prisma/client";
 import { PROVIDERS } from "@util/globals";
 import { deleteDrive } from "@util/helpers";
 import { Provider } from "@util/types";
-import gravatar from "gravatar";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -27,80 +27,67 @@ const Dashboard = () => {
   // };
 
   return (
-    <>
-      <Head>
-        <title>Your Drives | Firefiles</title>
-        <meta charSet="utf-8" />
-      </Head>
-      <div className="flex flex-col">
-        <Navbar />
-        <div className="my-4 sm:flex-col md:mx-8 mx4">
-          <Image
-            src={gravatar.url(user?.email, {
-              s: "110",
-              protocol: "https",
-            })}
-            alt="Profile Picture"
-            width={110}
-            height={110}
-            className="rounded-full ml-4"
-          />
-          <div className="ml-4">
-            <h1 className="text-2xl font-semibold">
-              👋 Hello There!
-            </h1>
-            <div className='flex items-baseline'>
-              <p>
-                <strong>Your Email:</strong> {user?.email}
-              </p>
-            </div>
-            <div className="flex items-baseline">
-              <strong>Current Plan: </strong>
-              <span className="inline-block bg-purple-500 text-white rounded-full py-1 px-3 ml-1">
-                {user?.plan} Plan
-              </span>
-              <button className="ml-1">
-                Upgrade
-              </button>
-            </div>
-            <button>View Payment Settings</button>
-          </div>
-        </div>
-        <hr />
-        <div className="mx-4 md:mx-8">
-          <h1 className="text-3xl font-semibold my-3">
-            Your Drives
-          </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {!data && isValidating ? (
-              <>
-                <Skeleton height="140px" width="100%" borderRadius="lg" />
-                <Skeleton height="140px" width="100%" borderRadius="lg" />
-                <Skeleton height="140px" width="100%" borderRadius="lg" />
-                <Skeleton height="140px" width="100%" borderRadius="lg" />
-              </>
-            ) : (
-              data?.map((drive) => (
-                <div
-                  key={drive.id}
-                  className="hoverAnim cursor-pointer flex-col items-center rounded-lg shadow-lg w-full h-[140px] border transition-all duration-100 ease-in-out"
-                >
-                  <div className="flex-1 w-full mt-2" onClick={() => router.push(`/drives/${drive.id}`)}>
-                    <Image
-                      alt="Provider Logo"
-                      src={PROVIDERS.filter((p) => p.id === drive.type)[0].logo}
-                      width={90}
-                      className="m-auto"
-                    />
-                  </div>
-                  <div className="flex p-2 w-full justify-between items-center">
-                    <p
-                      onClick={() => router.push(`/drives/${drive.id}`)}
-                      className="flex-1 truncate text-sm text-left px-2"
-                    >
-                      {drive.name}
-                    </p>
-                    <OptionsPopover header={drive.name}>
+		<Layout>
+			<Head>
+				<title>Your Drives | Firefiles</title>
+				<meta charSet="utf-8" />
+			</Head>
+			<div className="flex flex-col">
+				<div className="my-4 sm:flex-col md:mx-8 mx4">
+					<div className="ml-4">
+						<h1 className="text-2xl font-semibold">👋 Hello There!</h1>
+						<div className="flex items-baseline">
+							<p>
+								<strong>Your Email:</strong> {user?.email}
+							</p>
+						</div>
+						<div className="flex items-baseline">
+							<strong>Current Plan: </strong>
+							<span className="inline-block bg-purple-500 text-white rounded-full py-1 px-3 ml-1">
+								{user?.plan} Plan
+							</span>
+							<button className="ml-1">Upgrade</button>
+						</div>
+						<button>View Payment Settings</button>
+					</div>
+				</div>
+				<hr />
+				<div className="mx-4 md:mx-8">
+					<h1 className="text-3xl font-semibold my-3">Your Drives</h1>
+					<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+						{!data && isValidating ? (
+							<>
+								<Skeleton height="140px" width="100%" borderRadius="lg" />
+								<Skeleton height="140px" width="100%" borderRadius="lg" />
+								<Skeleton height="140px" width="100%" borderRadius="lg" />
+								<Skeleton height="140px" width="100%" borderRadius="lg" />
+							</>
+						) : (
+							data?.map((drive) => (
+								<div
+									key={drive.id}
+									className="hoverAnim cursor-pointer flex-col items-center rounded-lg shadow-lg w-full h-[140px] border transition-all duration-100 ease-in-out"
+								>
+									<div
+										className="flex-1 w-full mt-2"
+										onClick={() => router.push(`/drives/${drive.id}`)}
+									>
+										<Image
+											alt="Provider Logo"
+											src={PROVIDERS.filter((p) => p.id === drive.type)[0].logo}
+											width={90}
+											height={90}
+											className="m-auto"
+										/>
+									</div>
+									<div className="flex p-2 w-full justify-between items-center">
+										<p
+											onClick={() => router.push(`/drives/${drive.id}`)}
+											className="flex-1 truncate text-sm text-left px-2"
+										>
+											{drive.name}
+										</p>
+										{/* <OptionsPopover header={drive.name}>
                       <div className="items-stretch flex-col">
                         <div
                           // {...optionProps}
@@ -114,17 +101,17 @@ const Dashboard = () => {
                           <p className="ml-2">Delete Drive</p>
                         </div>
                       </div>
-                    </OptionsPopover>
-                  </div>
-                </div>
-              ))
-            )}
-            <AddDriveButton />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                    </OptionsPopover> */}
+									</div>
+								</div>
+							))
+						)}
+						<AddDriveButton />
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export default Dashboard;
