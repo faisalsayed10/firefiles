ba17e64f7085564c80f46acedcaa08e231688171
faisalsayed10 <faisal.sayed502@gmail.com>
2023-05-07 19:40:36 +0530

    :bug: (migration) Migrate error page to tailwind

--- apps/web/pages/error.tsx
+++ apps/web/pages/error.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React from "react";
+import { ErrorIcon } from "react-hot-toast";
+import { AlertCircle } from "tabler-icons-react";
 
 const Error = () => {
 	const router = useRouter();
@@ -13,16 +15,25 @@ const Error = () => {
 			<div className="flex items-center justify-center min-h-screen">
 				<div className="w-full max-w-md p-4">
 					<h2 className="text-2xl font-semibold text-center mb-4">An Unexpected Error Occurred</h2>
-					{/* <Alert status="error" fontSize="md" mb="6">
-						<AlertIcon /> */}
-					{router.query.message}
-					{/* </Alert> */}
-					<p className="text-center mb-4">
-						<Link href="https://firefiles.app/docs" target="_blank">
-							Make sure you're not missing anything
+					<p className="bg-red-100 p-4 rounded-md flex items-start justify-center gap-2 mb-2">
+						<div>
+							<AlertCircle width={26} height={26} className="text-red-600" />
+						</div>
+						<span>{router.query.message}</span>
+					</p>
+					<p className="text-center mb-1">
+						<Link
+							href="https://firefiles.app/docs"
+							target="_blank"
+							className="underline text-indigo-600 text-sm"
+						>
+							Read the docs
 						</Link>
 					</p>
-					<button className="w-full" onClick={() => router.push("/")}>
+					<button
+						className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50 disabled:cursor-not-allowed"
+						onClick={() => router.push("/")}
+					>
 						Back to Home
 					</button>
 				</div>
