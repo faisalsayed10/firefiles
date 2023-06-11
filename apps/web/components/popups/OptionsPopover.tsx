47ee944b8d3944a563e098bf6ea5cc57b56eca66
faisalsayed10 <faisal.sayed502@gmail.com>
2023-06-11 14:46:12 +0530

    :sparkles: (popover) options popover rewritten in tailwind

--- apps/web/components/popups/OptionsPopover.tsx
+++ apps/web/components/popups/OptionsPopover.tsx
@@ -1,48 +1,41 @@
-import { Popover, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Fragment, PropsWithChildren } from "react";
+import React from "react";
+import * as Popover from "@radix-ui/react-popover";
+import { DotsVertical } from "tabler-icons-react";
 
 interface Props {
-	header: string;
-	footer?: string;
+	options: {
+		name: string;
+		icon: React.ReactNode;
+		onClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+	}[];
 }
 
-const OptionsPopover: React.FC<PropsWithChildren<Props>> = ({ header, footer, children }) => {
+const OptionsPopover: React.FC<Props> = ({ options }) => {
 	return (
-		<div className="fixed top-16 w-full max-w-sm px-4">
-			<Popover className="relative">
-				{({ open }) => (
-					<>
-						<Popover.Button
-							className={`
-                ${open ? "" : "text-opacity-90"}
-                group inline-flex items-center rounded-md bg-orange-700 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+		<Popover.Root>
+			<Popover.Trigger>
+				<DotsVertical />
+			</Popover.Trigger>
+
+			<Popover.Portal>
+				<Popover.Content
+					className="animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit rounded-md border bg-white py-1.5 text-black shadow-md outline-none"
+					sideOffset={5}
+					align="start"
+				>
+					{options.map((option) => (
+						<div
+							className="cursor-pointer flex items-center justify-center gap-2 bg-white px-2 py-1.5 hover:bg-gray-100 transition-colors duration-100 ease-in-out"
+							key={option.name}
+							onClick={option.onClick}
 						>
-							<span>{header}</span>
-							<ChevronDownIcon
-								className={`${open ? "" : "text-opacity-70"}
-                  ml-2 h-5 w-5 text-orange-300 transition duration-150 ease-in-out group-hover:text-opacity-80`}
-								aria-hidden="true"
-							/>
-						</Popover.Button>
-						<Transition
-							as={Fragment}
-							enter="transition ease-out duration-200"
-							enterFrom="opacity-0 translate-y-1"
-							enterTo="opacity-100 translate-y-0"
-							leave="transition ease-in duration-150"
-							leaveFrom="opacity-100 translate-y-0"
-							leaveTo="opacity-0 translate-y-1"
-						>
-							<Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
-								{children}
-							</Popover.Panel>
-							{footer}
-						</Transition>
-					</>
-				)}
-			</Popover>
-		</div>
+							{option.icon}
+							{option.name}
+						</div>
+					))}
+				</Popover.Content>
+			</Popover.Portal>
+		</Popover.Root>
 	);
 };
 
