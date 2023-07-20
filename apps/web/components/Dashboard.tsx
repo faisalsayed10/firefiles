fc2c6f9e25437ba950cf1face9327af1d13cc022
ConlynPattison <cpattison@csumb.edu>
2023-07-19 17:19:07 -0700

    Added: Altering the Drive UI based on User Role
    The UI components which will result in writing actions to the drive
    currently open will now react to the value of provided RoleContext passed
    from a resulting server query. Indicators for the role of the user in the open
    drive and tooltips for 'disabled' buttons/components will also be added in
    later commits.

--- apps/web/components/Dashboard.tsx
+++ apps/web/components/Dashboard.tsx
@@ -5,7 +5,7 @@ import Navbar from "@components/ui/Navbar";
 import useBucket from "@hooks/useBucket";
 import useKeys from "@hooks/useKeys";
 import { Provider, FileSortConfig } from "@util/types";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import Dropzone from "react-dropzone";
 import LoadingOverlay from "react-loading-overlay";
 import UploadProgress from "./files/UploadProgress";
@@ -13,151 +13,159 @@ import GridView from "./GridView";
 import ListView from "./ListView";
 import { DriveFile } from "@util/types";
 import { sortDriveFiles } from "@util/file-sorting";
+import { RoleContext } from "pages/drives/[id]";
+import { Role } from "@prisma/client";
 
 const baseStyle = {
-	outline: "none",
-	transition: "border .2s ease-in-out",
+  outline: "none",
+  transition: "border .2s ease-in-out",
 };
 
 const activeStyle = {
-	borderWidth: 2,
-	borderRadius: 2,
-	borderStyle: "dashed",
-	borderColor: "#2196f3",
-	backgroundColor: "rgba(0, 0, 0, 0.25)",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderStyle: "dashed",
+  borderColor: "#2196f3",
+  backgroundColor: "rgba(0, 0, 0, 0.25)",
 };
 
 const Dashboard = () => {
-	const [draggedFilesToUpload, setDraggedFilesToUpload] = useState<File[]>([]);
-	const [isDragging, setIsDragging] = useState(false);
-	const [isFolderDeleting, setIsFolderDeleting] = useState(false);
-	const { currentFolder, files, folders, loading, uploadingFiles } = useBucket();
-	const { colorMode } = useColorMode();
-	const style = useMemo(() => ({ ...baseStyle, ...(isDragging ? activeStyle : {}) }), [isDragging]);
-	const [gridView, setGridView] = useState(false);
-	const [fileSort, setFileSort] = useState<FileSortConfig>({ property: "name", isAscending: true });
-	const [sortedFiles, setSortedFiles] = useState<DriveFile[]>([]);
+  const [draggedFilesToUpload, setDraggedFilesToUpload] = useState<File[]>([]);
+  const [isDragging, setIsDragging] = useState(false);
+  const [isFolderDeleting, setIsFolderDeleting] = useState(false);
+  const { currentFolder, files, folders, loading, uploadingFiles } = useBucket();
+  const { colorMode } = useColorMode();
+  const style = useMemo(() => ({ ...baseStyle, ...(isDragging ? activeStyle : {}) }), [isDragging]);
+  const [gridView, setGridView] = useState(false);
+  const [fileSort, setFileSort] = useState<FileSortConfig>({ property: "name", isAscending: true });
+  const [sortedFiles, setSortedFiles] = useState<DriveFile[]>([]);
+  const role = useContext(RoleContext);
 
-	useEffect(() => {
-		const storedView = localStorage.getItem("grid_view");
-		const storedFileSort = localStorage.getItem("file_sort");
+  useEffect(() => {
+    const storedView = localStorage.getItem("grid_view");
+    const storedFileSort = localStorage.getItem("file_sort");
 
-		if (storedView) setGridView(storedView === "true");
-		if (storedFileSort) setFileSort(JSON.parse(storedFileSort));
-	}, []);
+    if (storedView) setGridView(storedView === "true");
+    if (storedFileSort) setFileSort(JSON.parse(storedFileSort));
+  }, []);
 
-	useEffect(() => {
-		localStorage.setItem("grid_view", gridView.toString());
-	}, [gridView]);
+  useEffect(() => {
+    localStorage.setItem("grid_view", gridView.toString());
+  }, [gridView]);
 
-	useEffect(() => {
-		localStorage.setItem("file_sort", JSON.stringify(fileSort));
-	}, [fileSort]);
+  useEffect(() => {
+    localStorage.setItem("file_sort", JSON.stringify(fileSort));
+  }, [fileSort]);
 
-	useEffect(() => {
-		if (!files) {
-			setSortedFiles([]);
-			return;
-		}
-		const sortedFiles = sortDriveFiles(files, fileSort);
-		setSortedFiles(sortedFiles);
-	}, [fileSort, files])
+  useEffect(() => {
+    if (!files) {
+      setSortedFiles([]);
+      return;
+    }
+    const sortedFiles = sortDriveFiles(files, fileSort);
+    setSortedFiles(sortedFiles);
+  }, [fileSort, files]);
 
-	return (
-		<>
-			<LoadingOverlay
-				active={isFolderDeleting}
-				spinner={true}
-				text={`Deleting Files... \nPlease DO NOT close this tab.`}
-			>
-				<Dropzone
-					onDrop={(files) => {
-						setDraggedFilesToUpload(files);
-						setIsDragging(false);
-					}}
-					noClick
-					onDragOver={() => setIsDragging(true)}
-					onDragLeave={() => setIsDragging(false)}
-				>
-					{({ getRootProps, getInputProps }) => (
-						<Box
-							{...getRootProps({
-								style,
-							})}
-							minH="93vh"
-						>
-							<input {...getInputProps()} />
-							<Text
-								hidden={!isDragging}
-								fontSize={["2xl", "3xl", "3xl"]}
-								opacity="0.9"
-								color={colorMode === "light" ? "gray.700" : "gray.300"}
-								fontWeight="700"
-								align="center"
-								pos="absolute"
-								top="50%"
-								left="50%"
-								w="full"
-								transform="translate(-50%, -50%)"
-								p="0"
-								px="2"
-								m="0"
-							>
-								DROP FILES ANYWHERE ON THE SCREEN
-							</Text>
-							<Navbar />
-							<FolderBreadCrumbs currentFolder={currentFolder} />
-							<Divider />
-							{!gridView ? (
-								<ListView
-									loading={loading}
-									currentFolder={currentFolder}
-									files={sortedFiles}
-									folders={folders}
-									setGridView={setGridView}
-									setIsFolderDeleting={setIsFolderDeleting}
-									setFileSort={setFileSort}
-									fileSort={fileSort}
-								/>
-							) : (
-								<GridView
-									loading={loading}
-									currentFolder={currentFolder}
-									files={sortedFiles}
-									folders={folders}
-									setGridView={setGridView}
-									setIsFolderDeleting={setIsFolderDeleting}
-									setFileSort={setFileSort}
-									fileSort={fileSort}
-								/>
-							)}
-						</Box>
-					)}
-				</Dropzone>
-				<UploadFileButton
-					filesToUpload={draggedFilesToUpload}
-					setFilesToUpload={setDraggedFilesToUpload}
-				/>
-			</LoadingOverlay>
-			{uploadingFiles.length > 0 && (
-				<Center>
-					<Box
-						borderRadius="sm"
-						px="4"
-						pos="fixed"
-						bottom="5%"
-						width={["90vw", "60vw", "60vw"]}
-						boxShadow="3.8px 4.1px 6.3px -1.7px rgba(0, 0, 0, 0.2)"
-						backgroundColor={colorMode === "dark" ? "gray.700" : "white"}
-					>
-						{uploadingFiles.map((uploading) => (
-							<UploadProgress key={uploading.id} file={uploading} />
-						))}
-					</Box>
-				</Center>
-			)}
-		</>
-	);
+  return (
+    <>
+      <LoadingOverlay
+        active={isFolderDeleting}
+        spinner={true}
+        text={`Deleting Files... \nPlease DO NOT close this tab.`}
+      >
+        <Dropzone
+          disabled={role === Role.VIEWER}
+          onDrop={(files) => {
+            setDraggedFilesToUpload(files);
+            setIsDragging(false);
+          }}
+          noClick
+          onDragOver={() => setIsDragging(true)}
+          onDragLeave={() => setIsDragging(false)}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <Box
+              {...getRootProps({
+                style,
+              })}
+              minH="93vh"
+            >
+              <input {...getInputProps()} />
+              <Text
+                hidden={!isDragging}
+                fontSize={["2xl", "3xl", "3xl"]}
+                opacity="0.9"
+                color={colorMode === "light" ? "gray.700" : "gray.300"}
+                fontWeight="700"
+                align="center"
+                pos="absolute"
+                top="50%"
+                left="50%"
+                w="full"
+                transform="translate(-50%, -50%)"
+                p="0"
+                px="2"
+                m="0"
+              >
+                DROP FILES ANYWHERE ON THE SCREEN
+              </Text>
+              <Navbar />
+              <FolderBreadCrumbs currentFolder={currentFolder} />
+              <Divider />
+              {!gridView ? (
+                <ListView
+                  loading={loading}
+                  currentFolder={currentFolder}
+                  files={sortedFiles}
+                  folders={folders}
+                  setGridView={setGridView}
+                  setIsFolderDeleting={setIsFolderDeleting}
+                  setFileSort={setFileSort}
+                  fileSort={fileSort}
+                />
+              ) : (
+                <GridView
+                  loading={loading}
+                  currentFolder={currentFolder}
+                  files={sortedFiles}
+                  folders={folders}
+                  setGridView={setGridView}
+                  setIsFolderDeleting={setIsFolderDeleting}
+                  setFileSort={setFileSort}
+                  fileSort={fileSort}
+                />
+              )}
+            </Box>
+          )}
+        </Dropzone>
+        {role === Role.VIEWER ? (
+          ""
+        ) : (
+          <UploadFileButton
+            filesToUpload={draggedFilesToUpload}
+            setFilesToUpload={setDraggedFilesToUpload}
+          />
+        )}
+      </LoadingOverlay>
+      {uploadingFiles.length > 0 && (
+        <Center>
+          <Box
+            borderRadius="sm"
+            px="4"
+            pos="fixed"
+            bottom="5%"
+            width={["90vw", "60vw", "60vw"]}
+            boxShadow="3.8px 4.1px 6.3px -1.7px rgba(0, 0, 0, 0.2)"
+            backgroundColor={colorMode === "dark" ? "gray.700" : "white"}
+          >
+            {uploadingFiles.map((uploading) => (
+              <UploadProgress key={uploading.id} file={uploading} />
+            ))}
+          </Box>
+        </Center>
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
--- apps/web/components/GridView.tsx
+++ apps/web/components/GridView.tsx
@@ -1,78 +1,84 @@
 import { Box, Flex, Grid, IconButton, Skeleton, Text } from "@chakra-ui/react";
 import Folder from "@components/folders/Folder";
 import { DriveFile, DriveFolder } from "@util/types";
-import React from "react";
+import React, { useContext } from "react";
 import { LayoutList } from "tabler-icons-react";
 import { FileSortConfig } from "@util/types";
 import File from "./files/File";
 import AddFolderButton from "./folders/AddFolderButton";
 import FileSortMenu from "./ui/FileSortMenu";
+import { RoleContext } from "pages/drives/[id]";
+import { Role } from "@prisma/client";
 
 type Props = {
-	setGridView: React.Dispatch<React.SetStateAction<boolean>>;
-	loading: boolean;
-	currentFolder: DriveFolder;
-	folders: DriveFolder[];
-	files: DriveFile[];
-	setIsFolderDeleting: React.Dispatch<React.SetStateAction<boolean>>;
-	setFileSort: React.Dispatch<React.SetStateAction<FileSortConfig>>;
-	fileSort: FileSortConfig;
+  setGridView: React.Dispatch<React.SetStateAction<boolean>>;
+  loading: boolean;
+  currentFolder: DriveFolder;
+  folders: DriveFolder[];
+  files: DriveFile[];
+  setIsFolderDeleting: React.Dispatch<React.SetStateAction<boolean>>;
+  setFileSort: React.Dispatch<React.SetStateAction<FileSortConfig>>;
+  fileSort: FileSortConfig;
 };
 
 const GridView: React.FC<Props> = (props) => {
-	return (
-		<Box mx="4" mb="6">
-			<Flex align="center" justify="space-between" my="4">
-				<Text fontSize="3xl" fontWeight="600">
-					Your Files
-				</Text>
-				<Box>
-					<FileSortMenu setFileSort={props.setFileSort} fileSort={props.fileSort}/>
-					<IconButton aria-label="change-view" onClick={() => props.setGridView(false)}>
-						<LayoutList />
-					</IconButton>
-				</Box>
-			</Flex>
-			{props.loading ? (
-				<Grid
-					templateColumns={[
-						"repeat(auto-fill, minmax(140px, 1fr))",
-						"repeat(auto-fill, minmax(160px, 1fr))",
-						"repeat(auto-fill, minmax(160px, 1fr))",
-					]}
-					gap={[2, 6, 6]}
-					my="6"
-					mx="4"
-				>
-					<Skeleton h="140px" w="full" borderRadius="lg" />
-					<Skeleton h="140px" w="full" borderRadius="lg" />
-					<Skeleton h="140px" w="full" borderRadius="lg" />
-					<Skeleton h="140px" w="full" borderRadius="lg" />
-				</Grid>
-			) : (
-				<Grid
-					templateColumns={[
-						"repeat(auto-fill, minmax(140px, 1fr))",
-						"repeat(auto-fill, minmax(160px, 1fr))",
-						"repeat(auto-fill, minmax(160px, 1fr))",
-					]}
-					gap={[2, 6, 6]}
-				>
-					<AddFolderButton key="firefiles-add-folder-btn" currentFolder={props.currentFolder} />
-					{props.folders?.length > 0 &&
-						props.folders?.map((folder) => (
-							<Folder
-								key={folder.name}
-								setIsFolderDeleting={props.setIsFolderDeleting}
-								folder={folder}
-							/>
-						))}
-					{props.files?.length > 0 &&
-						props.files?.map((file) => <File key={file.name} file={file} gridView={true} />)}
-				</Grid>
-			)}
-		</Box>
-	);
+  const role = useContext(RoleContext);
+
+  return (
+    <Box mx="4" mb="6">
+      <Flex align="center" justify="space-between" my="4">
+        <Text fontSize="3xl" fontWeight="600">
+          Your Files
+        </Text>
+        <Box>
+          <FileSortMenu setFileSort={props.setFileSort} fileSort={props.fileSort} />
+          <IconButton aria-label="change-view" onClick={() => props.setGridView(false)}>
+            <LayoutList />
+          </IconButton>
+        </Box>
+      </Flex>
+      {props.loading ? (
+        <Grid
+          templateColumns={[
+            "repeat(auto-fill, minmax(140px, 1fr))",
+            "repeat(auto-fill, minmax(160px, 1fr))",
+            "repeat(auto-fill, minmax(160px, 1fr))",
+          ]}
+          gap={[2, 6, 6]}
+          my="6"
+          mx="4"
+        >
+          <Skeleton h="140px" w="full" borderRadius="lg" />
+          <Skeleton h="140px" w="full" borderRadius="lg" />
+          <Skeleton h="140px" w="full" borderRadius="lg" />
+          <Skeleton h="140px" w="full" borderRadius="lg" />
+        </Grid>
+      ) : (
+        <Grid
+          templateColumns={[
+            "repeat(auto-fill, minmax(140px, 1fr))",
+            "repeat(auto-fill, minmax(160px, 1fr))",
+            "repeat(auto-fill, minmax(160px, 1fr))",
+          ]}
+          gap={[2, 6, 6]}
+        >
+          {role === Role.VIEWER || (
+            <AddFolderButton key="firefiles-add-folder-btn" currentFolder={props.currentFolder} />
+          )}
+          {props.folders?.length > 0 &&
+            props.folders?.map((folder) => (
+              <Folder
+                key={folder.name}
+                setIsFolderDeleting={props.setIsFolderDeleting}
+                folder={folder}
+              />
+            ))}
+          {props.files?.length > 0 &&
+            props.files?.map((file) => <File key={file.name} file={file} gridView={true} />)}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default GridView;
