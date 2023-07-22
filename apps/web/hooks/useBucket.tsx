74c5f56ccaf9ef673491d33438e6e7c6b6d49e54
faisalsayed10 <faisal.sayed502@gmail.com>
2023-07-22 23:07:25 +0530

    feat: Edit useBucket to support cloudflare

--- apps/web/hooks/useBucket.tsx
+++ apps/web/hooks/useBucket.tsx
@@ -4,36 +4,38 @@ import useKeys from "./useKeys";
 import useS3 from "./useS3";
 
 export type ContextValue = {
-	loading: boolean;
-	currentFolder: DriveFolder;
-	folders: DriveFolder[];
-	files: DriveFile[];
-	uploadingFiles: UploadingFile[];
-	setUploadingFiles: React.Dispatch<React.SetStateAction<UploadingFile[]>>;
-	addFolder: (name: string) => void;
-	removeFolder: (folder: DriveFolder) => Promise<void>;
-	addFile: (files: File[] | FileList) => Promise<any>;
-	removeFile: (file: DriveFile) => Promise<boolean>;
+  loading: boolean;
+  currentFolder: DriveFolder;
+  folders: DriveFolder[];
+  files: DriveFile[];
+  uploadingFiles: UploadingFile[];
+  setUploadingFiles: React.Dispatch<React.SetStateAction<UploadingFile[]>>;
+  addFolder: (name: string) => void;
+  removeFolder: (folder: DriveFolder) => Promise<void>;
+  addFile: (files: File[] | FileList) => Promise<any>;
+  removeFile: (file: DriveFile) => Promise<boolean>;
 };
 
 export const ROOT_FOLDER: DriveFolder = {
-	name: "",
-	fullPath: "",
-	parent: null,
-	bucketName: null,
-	bucketUrl: null,
+  name: "",
+  fullPath: "",
+  parent: null,
+  bucketName: null,
+  bucketUrl: null,
 };
 
 export default function useBucket(): ContextValue {
-	const { keys } = useKeys();
+  const { keys } = useKeys();
 
-	if ((Provider[keys.type] as Provider) === Provider.firebase) {
-		return useFirebase();
-	} else if ((Provider[keys.type] as Provider) === Provider.s3) {
-		return useS3();
-	} else if ((Provider[keys.type] as Provider) === Provider.backblaze) {
-		return useS3();
-	}
+  if ((Provider[keys.type] as Provider) === Provider.firebase) {
+    return useFirebase();
+  } else if ((Provider[keys.type] as Provider) === Provider.s3) {
+    return useS3();
+  } else if ((Provider[keys.type] as Provider) === Provider.backblaze) {
+    return useS3();
+  } else if ((Provider[keys.type] as Provider) === Provider.cloudflare) {
+    return useS3();
+  }
 
-	return null;
+  return null;
 }
