8705438b94fc48985f2f04d46d992004e9d70c99
faisalsayed10 <faisal.sayed502@gmail.com>
2023-07-22 23:07:25 +0530

    Resolve merge conflict

--- apps/web/hooks/useBucket.tsx
+++ apps/web/hooks/useBucket.tsx
@@ -4,24 +4,24 @@ import useKeys from "./useKeys";
 import useS3 from "./useS3";
 
 export type ContextValue = {
-	loading: boolean;
-	currentFolder: DriveFolder;
-	folders: DriveFolder[];
-	files: DriveFile[];
-	uploadingFiles: UploadingFile[];
-	setUploadingFiles: React.Dispatch<React.SetStateAction<UploadingFile[]>>;
-	addFolder: (name: string) => void;
-	removeFolder: (folder: DriveFolder) => Promise<void>;
-	addFile: (files: File[] | FileList) => Promise<any>;
-	removeFile: (file: DriveFile) => Promise<boolean>;
+  loading: boolean;
+  currentFolder: DriveFolder;
+  folders: DriveFolder[];
+  files: DriveFile[];
+  uploadingFiles: UploadingFile[];
+  setUploadingFiles: React.Dispatch<React.SetStateAction<UploadingFile[]>>;
+  addFolder: (name: string) => void;
+  removeFolder: (folder: DriveFolder) => Promise<void>;
+  addFile: (files: File[] | FileList) => Promise<any>;
+  removeFile: (file: DriveFile) => Promise<boolean>;
 };
 
 export const ROOT_FOLDER: DriveFolder = {
-	name: "",
-	fullPath: "",
-	parent: null,
-	bucketName: null,
-	bucketUrl: null,
+  name: "",
+  fullPath: "",
+  parent: null,
+  bucketName: null,
+  bucketUrl: null,
 };
 
 export default function useBucket(): ContextValue {
