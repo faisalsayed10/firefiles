a24ff3c9028203f841d68e05d2789f72af339bbe
Hien Tran <85942569+hientrn1201@users.noreply.github.com>
2023-08-07 18:37:46 -0400

    Add invitation api

--- apps/web/util/emailSender/resendEmailSender.ts
+++ apps/web/util/emailSender/resendEmailSender.ts
@@ -1,6 +1,6 @@
-import { EmailSender, LoginEmailOptions } from "./emailSender";
+import { EmailSender, LoginEmailOptions, InvitationEmailOptions } from "./emailSender";
 import { Resend } from "resend";
-import { loginHtml, loginText } from "./format";
+import { loginHtml, loginText, inviteHtml, inviteText } from "./format";
 
 export class ResendEmailSender implements EmailSender {
   async sendLoginEmail(options: LoginEmailOptions): Promise<void> {
@@ -12,7 +12,20 @@ export class ResendEmailSender implements EmailSender {
       html: loginHtml(`${url}/api/auth/verify/${token}`, email),
       text: loginText(`${url}/api/auth/verify/${token}`, email),
       subject: "Log in to Firefiles",
-      tags: [{ name: "firefiles", value: "login_link" }],
+      tags: [{ name: "firefiles", value: "invitation" }],
+    });
+  }
+
+  async sendInvitationEmail(options: InvitationEmailOptions): Promise<void> {
+    const { email, url } = options;
+    const resend = new Resend(process.env.RESEND_API_KEY || "re_123");
+    await resend.emails.send({
+      from: process.env.EMAIL_FROM,
+      to: email,
+      html: inviteHtml(url, email),
+      text: inviteText(url, email),
+      subject: "Invitation to a Bucket",
+      tags: [{ name: "firefiles", value: "invitation" }],
     });
   }
 }
